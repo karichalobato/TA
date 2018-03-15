2fd3d31db9437688a3534f63ceb20f1eae83d28c
UCA <edwin.morales9b@gmail.com>
2018-03-14 20:15:45 -0600

    PROYECTO BASE

---
+++ src/javaapplication4/JavaApplication4.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author UCA
+ */
+public class JavaApplication4 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO PRUEBA");
+    }
+    
+}
